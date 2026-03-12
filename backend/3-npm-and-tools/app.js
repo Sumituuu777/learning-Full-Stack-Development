1fb25181199485f4262e7220444f964f66adb98c
Sumit kumar <sumit.ydv.kumar2006@gmail.com>
2026-03-12 12:05:40 +0530

    took input from userand wrote it in a file

---
+++ backend/3-npm-and-tools/app.js
@@ -0,0 +1,66 @@
+const http=require('http');
+const fs=require('fs');
+const { URLSearchParams } = require('url');
+
+const requirefunction=(req,res)=>{
+
+    if(req.url==='/'){
+        res.setHeader('content-type','text/html');
+        res.write(`<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <title>Home</title>
+                </head>
+                <body>
+                    <form action="/submit-details" method="POST">
+                        <input type="text" placeholder="Enter your name" name="name" id="name">
+                        <input type="text" placeholder="Enter your age" name="age" id="age">
+                        <button type="submit">Submit</button>
+                        
+                    </form>
+                </body>
+                </html>`)
+    }
+    else if(req.url==='/submit-details' && req.method=="POST"){
+        const bufferArr=[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            bufferArr.push(chunk);
+        })
+        req.on('end',()=>{
+            const parsedbufferArr=Buffer.concat(bufferArr).toString();
+            console.log(parsedbufferArr);
+
+            const parsedpairs=new URLSearchParams(parsedbufferArr);
+
+            const jsonObj={};
+            for (const [key,value] of parsedpairs.entries()){
+                jsonObj[key]=value;
+            }
+            console.log(JSON.stringify(jsonObj))
+            console.log(jsonObj)
+            fs.writeFileSync('user-details.txt',JSON.stringify(jsonObj));
+        })
+    }
+    else if(req.url==='/products'){
+        res.setHeader('content-type','text/html');
+        res.write('<html>')
+        res.write('<h1>Products list...</h1>')
+        res.write('</html>')
+    }
+    else{
+        res.statusCode=404;
+        res.setHeader('content-type','text/html');
+        res.write('<html>')
+        res.write('<h1>Error 404:Page not found</h1>')
+        res.write('</html>')
+    }
+    res.end();
+    
+}
+const server=http.createServer(requirefunction);
+const PORT=3000;
+server.listen(PORT,()=>{
+    console.log(`server running at http://localhost:${PORT}/`);
+    
+});
